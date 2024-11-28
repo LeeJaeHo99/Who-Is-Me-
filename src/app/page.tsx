bcc8e1292a671418326d90b22ff7227ff4a8a39a
LeeJaeHo99 <apple@appleui-MacBookPro.local>
2024-11-29 00:04:44 +0900

    임시 커밋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
-
 export default function Home() {
     return (
         <div>
-            
+            <div className="main-wrap relative w-full overflow-hidden">
+                <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+                    
+                </div>
+            </div>
         </div>
     );
 }
